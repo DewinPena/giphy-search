083312ec7148b31a2c8dcc50b2f401dc262e7f2d
Dewin Pena <dewinx97@gmail.com>
2019-09-25 17:56:16 -0400

    Inital commit

---
+++ script.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+  
+
+  createGrid();
+
+  $( "#search" ).click(function() {
+    console.log("no")
+    getGif();
+  });
+
+
+  function getGif(){
+
+    var input = $("#search-input").val().trim();
+    input = input.replace(/ /g, "+");
+    var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + input + '&api_key=gUUfS6bKKE16jJ0CDTg90UuDDGj7kvyC&limit=28';
+    
+    var xhr = $.get(queryURL);
+
+      xhr.done(function(response) {
+        
+        console.log("success got data", response);
+      
+        var giff = response.data
+        
+        for(i in giff){
+
+          $('#g'+i).empty().append("<img src='"+giff[i].images.original.url+"'>");
+       
+           }
+      });
+    
+  }
+  
+  function createGrid(){
+    for(var i = 0; i < 28; i++){
+
+    var $newGifDiv = $("<div/>")
+    .attr("id", "g"+ i)
+    .addClass("column")
+    .addClass("is-3")
+    .addClass("has-text-centered")
+    .addClass("level")
+    .addClass("box-style");
+
+    $('#target-container').append($($newGifDiv))
+    }
+  }
+   
+  
+        
+});
